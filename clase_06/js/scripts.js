bd8acf5a57b4811ee16a61cc951586daa4e390c7
gusmalc <gustavo@malcotti.com>
2022-06-29 11:12:36 -0300

    1

---
+++ clase_06/js/scripts.js
@@ -0,0 +1,32 @@
+// let num1;
+// let num2;
+
+
+// num1 = 3;
+// num2 = 15;
+
+
+// let resultado = num1 + num2;
+
+
+// alert(resultado);
+
+// console.log(resultado);
+
+
+let edad;
+
+edad = 22;
+
+
+
+let respuesta;
+
+if(edad < 18){
+    respuesta = "Sos menor";
+}else{
+    respuesta = "Sos mayor";
+}
+
+
+alert(respuesta);
